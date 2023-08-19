eabf0a6c2dfb74f1f34ea58df2c820cd35ecf85b
M Sanjay Sharma <65958268+MSanjaySharma@users.noreply.github.com>
2023-08-19 22:58:33 +0530

    Update add-gh-pages.sh

--- add-gh-pages.sh
+++ add-gh-pages.sh
@@ -12,14 +12,17 @@ npm pkg set scripts.deploy="gh-pages -d dist"
 ########################################################################
 # vite.config.jsonf update
 viteConfig="
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from \"vite\";
+import react from \"@vitejs/plugin-react-swc\";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/$project_repo_name",
+  base: \"/$project_repo_name\",
   plugins: [react()],
 });
-
 "
-echo "$viteConfig" >vite.config.json
+echo "$viteConfig" >vite.config.ts
+
+########################################################################
+# run prettier
+npx prettier --write .
